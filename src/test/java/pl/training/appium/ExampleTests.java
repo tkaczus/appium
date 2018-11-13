3fece2815c71c312b7d173daaada6e4a2efc1f86
lukasztkaczyk@gmail.com <$Vfr45tgb>
2018-11-13 15:34:06 +0100

    koljne testy

--- src/test/java/pl/training/appium/ExampleTests.java
+++ src/test/java/pl/training/appium/ExampleTests.java
@@ -1,15 +1,33 @@
 package pl.training.appium;
 
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.offset.PointOption;
+import javafx.scene.shape.MoveTo;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
 
 public class ExampleTests {
 
@@ -17,6 +35,25 @@ public class ExampleTests {
     private final static String TEST_DEVICE_NAME = "Nexus";
     private final static String APPIUM_SERVER_URL = "http://127.0.0.1:4723/wd/hub";
     private String testNetwork = "Test network";
+
+    private AndroidDriver<AndroidElement> driver = getAndroidDriver();
+
+    @Before
+    public void beforeTest() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void afterTest() {
+        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("Screenshot.jpg"));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        driver.closeApp();
+    }
+
 
     /**
      * ustawiamy połaczenie do anroida (symulatora) - ta jest instalowana
@@ -41,12 +78,12 @@ public class ExampleTests {
 
     @Test
     public void runApplication() {
-        getAndroidDriver();
+        driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+        System.out.println("elements = " + driver.findElementsByAndroidUIAutomator("new UiSelector().clickable(true)").size());
     }
 
     @Test
     public void setupWifi() {
-        AndroidDriver<AndroidElement> driver = getAndroidDriver();
         driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
         driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
         driver.findElementById("android:id/checkbox").click();
@@ -56,6 +93,59 @@ public class ExampleTests {
         driver.findElementsByClassName("android.widget.Button").get(1).click();
         driver.findElementsByClassName("android.widget.RelativeLayout").get(1).click();
         Assert.assertEquals(testNetwork, driver.findElementByClassName("android.widget.EditText").getText());
-
     }
+
+    @Test
+    public void shouldShowContextMenuOnLongTap() {
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='Expandable Lists']").click();
+
+        AndroidElement customAdapter = driver.findElementByXPath("//android.widget.TextView[@text='1. Custom Adapter']");
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.tap(tapOptions().withElement(element(customAdapter))).perform();
+
+        AndroidElement dogNames = driver.findElementByXPath("//android.widget.TextView[@text='Dog Names']");
+        touchAction.longPress(longPressOptions().withElement(element(dogNames))
+                .withDuration(Duration.ofSeconds(2)))
+                .release()
+                .perform();
+
+        Assert.assertTrue(driver.findElementById("android:id/title").isEnabled());
+    }
+
+
+    @Test
+    public void shouldChangeTime() {
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='Date Widgets']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='1. Dialog']").click();
+        driver.findElementById("io.appium.android.apis:id/pickTime").click();
+
+        List<AndroidElement> elementyZegara = driver.findElementsByClassName("android.widget.RadialTimePickerView$RadialPickerTouchHelper");
+        elementyZegara.get(0).click();
+
+        TouchAction touchAction = new TouchAction(driver);
+
+        touchAction.longPress(longPressOptions().withElement(element(elementyZegara.get(5)))
+                .withDuration(Duration.ofSeconds(2)))
+                .moveTo(element(elementyZegara.get(0)))
+                .release()
+                .perform();
+
+        String h = driver.findElementById("android:id/hours").getText();
+        String m = driver.findElementById("android:id/minutes").getText();
+        Assert.assertEquals(h + ":" + m, "1:00");
+    }
+
+    @Test
+    public void shouldScrool() {
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        driver.findElementsByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Switches\"))");
+        driver.findElementByXPath("//android.widget.TextView[@text='Switches']").click();
+        driver.findElementByXPath("//android.widget.Switch[@text='Default is on ON']").click();
+
+        Assert.assertEquals(driver.findElementByXPath("//android.widget.Switch[@text='Default is on OFF']").getAttribute("checked"), "false");
+    }
+
+
 }
